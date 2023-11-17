d6465c5fc0d316dd9ee7a795406436251402ec62
Ankur Kumar <ankur4e@gmail.com>
2023-11-17 09:25:54 +0530

    Uploading to repo

--- src/Login.js
+++ src/Login.js
@@ -2,29 +2,116 @@ import React, { useState } from 'react';
 
 export default function Login() {
   const [name, setName] = useState(null);
+  const [faceEncodings, setFaceEncodings] = useState(null);
+  const [fingerData, setFingerData] = useState(null);
+  
   const [loggedIn, setLoggedIn] = useState(false);
 
-  const login = async () => {
+  const registerFace = async ()=> {
     try {
-      const response = await fetch("http://127.0.0.1:5000/login?name="+name, {
+      const response = await fetch("http://127.0.0.1:5000/registerFace", {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
         }
       });
-
+      
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-
+      
       const data = await response.json(); // Parse the JSON response
-      if(data.message === "Login Successful.")
-      {
-        setLoggedIn(true);  
-      }
-      console.log(data.message); // Log the message
-    } catch (error) {
+      setFaceEncodings(data.face_encodings) // Update the resultStatus state
+      console.log(data.face_encodings); // Log the message
+    }
+    catch (error) {
       console.error(error);
+    }
+  }
+
+  const inputFinger = async ()=>{
+    try {
+      const response = await fetch("http://127.0.0.1:5000/inputFinger", {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+  
+  const registerFinger = async ()=>{
+    try {
+      const response = await fetch("http://127.0.0.1:5000/registerFinger", {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      
+      const data = await response.json(); // Parse the JSON response
+      setFingerData(data) // Update the resultStatus state
+      console.log(data); // Log the message
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+
+  const login = async () => {
+    try {
+      // Check if name, faceEncodings, and fingerData are available
+      if (name && faceEncodings && fingerData) {
+        // Create an object with user data
+        const userData = {
+          name: name,
+          faceEncodings: faceEncodings,
+          fingerData: fingerData,
+        };
+  
+        // Make a POST request to the backend to store the user data
+        const response = await fetch('http://127.0.0.1:5000/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(userData), // Convert to JSON string
+        });
+  
+        
+        // To access the response data, you need to parse the JSON from the response
+        if (response.ok) {
+          const responseData = await response.json();
+          console.log(responseData.message)
+          if (responseData.message === "Login Successful.") {
+            // If face recognition is successful, set loggedIn to true
+            setLoggedIn(true);
+          } else {
+            // If face recognition is unsuccessful, handle the error
+            console.error('Failed to Log In');
+          }
+        } else {
+          // Handle network or server errors when the response is not ok (e.g., HTTP status code other than 2xx)
+          console.error('Failed to make the request. Status code:', response.status);
+        }
+      } else {
+        // Handle cases where required data is missing
+        console.error('Missing data (name, faceEncodings, or fingerData).');
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
     }
   };
 
@@ -36,25 +123,58 @@ export default function Login() {
   return (
     <>
       {loggedIn === false ? (
-        <div className="form-group">
-        <label>Enter your name</label>
-        <input
-            type="text"
-            name="name"
-            className="form-control"
-            onChange={(e) => {
+        <>
+      
+          <div className="form-group">
+            <label>Enter your name</label>
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              onChange={(e) => {
                 setName(e.target.value);
-            }}
+              }}
             />
-          <input
-            type="submit"
-            value="Authenticate Me"
-            onClick={() => {
-              login();
-            }}
-            className="btn btn-primary"
-          />
-        </div>
+          </div>
+          <div className="form-group">
+            <button
+              onClick={() => {
+                registerFace();
+              }}
+              className="btn btn-primary"
+            >
+              Take my Picture 
+            </button>
+          </div>
+          <div className="form-group">
+            <button
+              onClick={() => {
+                inputFinger();
+              }}
+              className="btn btn-primary"
+            >
+              Take My FingerScan
+            </button>
+            <button
+              onClick={() => {
+                registerFinger();
+              }}
+              className="btn btn-primary"
+            >
+              Store
+            </button>
+          </div>
+          <div className="form-group">
+            <button
+              onClick={() => {
+                login();
+              }}
+              className="btn btn-primary"
+            >
+              LogIn
+            </button>
+          </div>
+        </>
       ) : (
         <div>
           <div className="details">
--- src/Register.js
+++ src/Register.js
@@ -2,35 +2,112 @@ import React, { useState } from 'react';
 
 export default function Register() {
   const [name, setName] = useState(null);
-  const [resultStatus, setResultStatus] = useState(null);
+  const [faceEncodings, setFaceEncodings] = useState(null);
+  const [fingerData, setFingerData] = useState(null);
 
-  console.log("register called.");
-
-  const register = async () => {
+  
+  const registerFace = async ()=> {
     try {
-      const response = await fetch("http://127.0.0.1:5000/register?name="+name, {
+      const response = await fetch("http://127.0.0.1:5000/registerFace", {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
         }
       });
-
+      
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-
+      
       const data = await response.json(); // Parse the JSON response
-      setResultStatus(data.message); // Update the resultStatus state
-      console.log(data.message); // Log the message
-    } catch (error) {
+      setFaceEncodings(data.face_encodings) // Update the resultStatus state
+      console.log(data.face_encodings); // Log the message
+    }
+    catch (error) {
       console.error(error);
     }
-  };
+  }
 
+  const inputFinger = async ()=>{
+    try {
+      const response = await fetch("http://127.0.0.1:5000/inputFinger", {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+  
+  const registerFinger = async ()=>{
+    try {
+      const response = await fetch("http://127.0.0.1:5000/registerFinger", {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      
+      const data = await response.json(); // Parse the JSON response
+      setFingerData(data) // Update the resultStatus state
+      console.log(data); // Log the message
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  const register = async () => {
+    try {
+      // Check if name, faceEncodings, and fingerData are available
+      if (name && faceEncodings && fingerData) {
+        // Create an object with user data
+        const userData = {
+          name: name,
+          faceEncodings: faceEncodings,
+          fingerData: fingerData,
+        };
+  
+        // Make a POST request to the backend to store the user data
+        const response = await fetch('http://127.0.0.1:5000/registerUser', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(userData), // Convert to JSON string
+        });
+  
+        if (response.ok) {
+          // Data successfully stored in the database
+          console.log('User data stored successfully.');
+        } else {
+          // Handle errors if the request fails
+          console.error('Failed to store user data in the database.');
+        }
+      } else {
+        // Handle cases where required data is missing
+        console.error('Missing data (name, faceEncodings, or fingerData).');
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  };
+  
+  
   return (
     <>
-      {resultStatus !== "Done" ? (
-        <>
+      
           <div className="form-group">
             <label>Enter your name</label>
             <input
@@ -43,25 +120,43 @@ export default function Register() {
             />
           </div>
           <div className="form-group">
-            <input
-              type="submit"
-              value="Take My Picture"
+            <button
+              onClick={() => {
+                registerFace();
+              }}
+              className="btn btn-primary"
+            >
+              Take my Picture 
+            </button>
+          </div>
+          <div className="form-group">
+            <button
+              onClick={() => {
+                inputFinger();
+              }}
+              className="btn btn-primary"
+            >
+              Take My FingerScan
+            </button>
+            <button
+              onClick={() => {
+                registerFinger();
+              }}
+              className="btn btn-primary"
+            >
+              Store
+            </button>
+          </div>
+          <div className="form-group">
+            <button
               onClick={() => {
                 register();
               }}
               className="btn btn-primary"
-            />
+            >
+              Submit
+            </button>
           </div>
         </>
-      ) : (
-        <div>
-          <div className="details">
-            Hello {name}!
-            <br />
-            Your registration is successfully completed now you may proceed to login.
-          </div>
-        </div>
-      )}
-    </>
-  );
+  )
 }
